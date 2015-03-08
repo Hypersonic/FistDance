e1bda3f8061e9249148b75b90e3330f7c75d00f8
Zane Sterling <sterling.zane@gmail.com>
2015-03-08 01:42:42 -0500

    Clean up skel_parse

--- src/core/parser/skeleton/parser.cpp
+++ src/core/parser/skeleton/parser.cpp
@@ -11,63 +11,47 @@ void skel_parse(char *fn, Skeleton &skel) {
 
 	// scan each line
 	while (parse_split(fp, token_buffer)) {
-		if (strcmp(token_buffer[0], "node") == 0) {
-			if (cur_node < 0) {
+		if (cur_node < 0) {
+			if (strcmp(token_buffer[0], "node")) {
 				// first node
 				SkeletonNode &node = skel.nodes[skel.n_nodes++];
 				node.parent = -1; // indicate no parent
 				cur_node = 0;
 			} else {
-				// any other node
-				SkeletonNode &node = skel.nodes[cur_node];
-				int new_node = skel.n_nodes++;
-
-				node.push(new_node);
-				skel.nodes[new_node].parent = cur_node;
-				cur_node = new_node;
+				printf("error: tried to %s before making a node\n",
+				       token_buffer[0]);
 			}
+			continue;
+		}
+
+		if (strcmp(token_buffer[0], "node") == 0) {
+			// any other node
+			SkeletonNode &node = skel.nodes[cur_node];
+			int new_node = skel.n_nodes++;
+
+			node.push(new_node);
+			skel.nodes[new_node].parent = cur_node;
+			cur_node = new_node;
 		} else if (strcmp(token_buffer[0], "trans") == 0) {
-			if (cur_node < 0) {
-				printf("error: tried to translate before making a node\n");
-				continue;
-			}
-
 			// set node translate
 			SkeletonNode &node = skel.nodes[cur_node];
 			node.transform.trans.x = strtod(token_buffer[1], NULL);
 			node.transform.trans.y = strtod(token_buffer[2], NULL);
 		} else if (strcmp(token_buffer[0], "rot") == 0) {
-			if (cur_node < 0) {
-				printf("error: tried to rotate before making a node\n");
-				continue;
-			}
-
 			// set node rot
 			SkeletonNode &node = skel.nodes[cur_node];
 			sscanf(token_buffer[1], "%d", &node.transform.rot);
 		} else if (strcmp(token_buffer[0], "name") == 0) {
-			if (cur_node < 0) {
-				printf("error: tried to name before making a node\n");
-				continue;
-			}
-
-			// TODO add node naming
 		} else if (strcmp(token_buffer[0], "ascend") == 0) {
-			if (cur_node < 0) {
-				printf("error: tried to ascend before making a node\n");
-				continue;
-			} else if (cur_node == 0) {
+			if (cur_node == 0) {
 				printf("error: tried to ascend while at top node\n");
 				continue;
 			}
 
+			// ascend tree
 			cur_node = skel.nodes[cur_node].parent;
 		} else if (strcmp(token_buffer[0], "rad") == 0) {
-			if (cur_node < 0) {
-				printf("error: tried to set rad before making a node\n");
-				continue;
-			}
-
+			// set node radius
 			SkeletonNode &node = skel.nodes[cur_node];
 			node.info.hittable = HTBX_HTBX | HTBX_HTPT;
 			node.info.rad = strtod(token_buffer[1], NULL);
